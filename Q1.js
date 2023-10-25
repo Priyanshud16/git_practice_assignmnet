29eae07273f0db522f7327abe44f71f5b30c1e67
Priyanshud16 <priyanshuharrdy@gmail.com>
2023-10-25 15:42:21 +0530

    addedfile

--- Q1.js
+++ Q1.js
@@ -1,4 +1,6 @@
-let a=5
-let b=6
-
-console.log(a+c)
+const num = 17; // Replace with the number you want to check
+if (isPrime(num)) {
+  console.log(num + " is a prime number.");
+} else {
+  console.log(num + " is not a prime number.");
+}
